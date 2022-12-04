7624b830873c5e221cd206a23f928a0e7c805349
Victor Pan <victorpan2017@gmail.com>
2022-12-04 12:18:26 -0800

    day 4 part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,22 +1,53 @@
-use std::{fs};
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Should be able to read the input file.");
 
-    let num_pairs = input.lines()
-    .map(|line| line.split(','))
-    .map(|mut ranges| (ranges.next().unwrap(), ranges.next().unwrap()))
-    .map(|(range_a_str, range_b_str)| {
-        let mut range_a = range_a_str.split('-').map(|x| x.parse::<i32>().unwrap());
-        let mut range_b = range_b_str.split('-').map(|x| x.parse::<i32>().unwrap());
-        let (a, b, x, y) = (range_a.next(), range_a.next(), range_b.next(), range_b.next());
-        if a <= x && b >= y || a >= x && b <= y {
-            1
-        } else {
-            0
-        }
-    })
-    .sum::<i32>();
+    let range_pairs = input
+        .lines()
+        .map(|line| line.split(','))
+        .map(|mut ranges| (ranges.next().unwrap(), ranges.next().unwrap()))
+        .map(|(range_a_str, range_b_str)| {
+            let mut range_a = range_a_str.split('-').map(|x| x.parse::<i32>().unwrap());
+            let mut range_b = range_b_str.split('-').map(|x| x.parse::<i32>().unwrap());
+            (
+                range_a.next().unwrap(),
+                range_a.next().unwrap(),
+                range_b.next().unwrap(),
+                range_b.next().unwrap(),
+            )
+        })
+        .into_iter()
+        .collect::<Vec<(i32, i32, i32, i32)>>();
 
-    println!("{num_pairs}");
+    part_one(&range_pairs);
+    part_two(range_pairs);
+}
+
+fn part_one(range_pairs: &Vec<(i32, i32, i32, i32)>) {
+    let num_subsets = range_pairs
+        .iter()
+        .map(|(a, b, x, y)| {
+            if a <= x && b >= y || a >= x && b <= y {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<i32>();
+    println!("{num_subsets}");
+}
+
+fn part_two(range_pairs: Vec<(i32, i32, i32, i32)>) {
+    let num_overlap = range_pairs
+        .iter()
+        .map(|(a, b, x, y)| {
+            if x <= b && b <= y || a <= y && y <= b {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<i32>();
+    println!("{num_overlap}");
 }
